d9a091baef20fc73dd82b5f140edcedf53bb4fb3
Ausare <asain_baitasov@mail.ru>
2024-08-07 20:22:27 +0600

    add navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+      fontFamily: {
+        primary: ['Poppins']
+      }
     },
-  },
-  plugins: [],
+	},
+	plugins: [],
 };
-export default config;

dd8df698c2d6cd04d927a5d785adeaa70bdba686
Ausare <asain_baitasov@mail.ru>
2024-08-08 20:46:05 +0600

    add sections about services skills footer and animation aos

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,12 +6,12 @@ import { ArrowDownTrayIcon } from '@heroicons/react/20/solid';
 
 const Hero = () => {
   return (
-		<div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center ">
+		<div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center ">
 			<Particle />
 			<div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
 				<div>
 					<h1 className='text-[35px] md:text-[50px] text-white font-bold'>
-						HI, I'M <span className='text-yellow-400'>DEVELOPER!</span>
+						HI, I <span className='text-yellow-400'>DEVELOPER!</span>
 					</h1>
 					<TextEffect />
 					<p className='mt-[2rem] text-[18px] text-[#ffffff92]'>
@@ -27,7 +27,7 @@ const Hero = () => {
             
 					</div>
 				</div>
-				<div className='w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]'>
+				<div  className='w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]'>
 					<Image
 						src='/images/u1.jpg'
 						alt='user'

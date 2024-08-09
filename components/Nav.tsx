b013b533e80905e0397a066bf9ac51f492915f85
Ausare <asain_baitasov@mail.ru>
2024-08-09 17:32:45 +0600

    rework hero and nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,11 +12,10 @@ const Nav = ({openNav}:Props) => {
             <span className='text-yellow-300'>DEV</span>
 
         </h1>
-        <div className='nav-link'>Home</div>
+        <div className='nav-link'>HOME</div>
         <div className='nav-link'>SERVICES</div>
         <div className='nav-link'>ABOUT</div>
         <div className='nav-link'>PROJECT</div>
-        <div className='nav-link'>BLOG</div>
         <div className='nav-link'>CONTACT</div>
         <div onClick={openNav}>
             <Bars3Icon className='w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300 '/>
--- components/TextEffect.tsx
+++ components/TextEffect.tsx
@@ -7,16 +7,14 @@ const TextEffect = () => {
 				// Same substring at the start will only be typed out once, initially
 				'Coder',
 				1500, // wait 1s before replacing "Mice" with "Hamsters"
-				'Web developer',
-				1500,
-				'Designer',
+				'Developer',
 				1500,
 				'Programmer',
 				1500,
 			]}
 			
 			speed={50}
-			className='text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase'
+			className='text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase text-center'
 			repeat={Infinity}
 		/>
 	);
